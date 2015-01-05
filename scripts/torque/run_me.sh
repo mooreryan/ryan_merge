53fb13783557c784e0dff0c1fcae29dbd72c550b
Ryan Moore <moorer@udel.edu>
2015-01-05 10:10:12 -0500

    Add cleanup steps

--- scripts/torque/run_me.sh
+++ scripts/torque/run_me.sh
@@ -22,7 +22,18 @@ time ruby $scripts/dereplicate_fast_2.rb -p $prefix -o $outdir /home/aoh/cow/vir
     && \
     time ruby $scripts/celera.rb -f $short --make-fq -p Sample_100.short -o $outdir/celera_assembly \
     && \
-    time ruby $scripts/minimus2.rb --celera $outdir/celera_assembly/assembly_celera/9-terminator/Sample_100.short.ctg.fasta --long $long -o $outdir
+    time ruby $scripts/minimus2.rb --celera $outdir/celera_assembly/assembly_celera/9-terminator/Sample_100.short.ctg.fasta --long $long -o $outdir \
+    && \
+    time ruby $scripts/make_minimus_headers.rb $outdir/two_assemblies.fa > $outdir/tmp && mv $outdir/tmp $outdir/two_assemblies.fa \
+    && \
+    time /usr/local/AMOS/bin/toAmos -s $outdir/two_assemblies.fa -o $outdir/two_assemblies.fa.afg \
+    && \
+    time /home/moorer/bin/minimus2 $outdir/two_assemblies.fa -D REFCOUNT=21108 -D OVERLAP=80 -D CONSERR=0.06 -D MINID=98 \
+    && \
+    time cat $outdir/two_assemblies.fa.fasta $outdir/two_assemblies.fa.singletons.seq > $outdir/two_assemblies.ryan_merged.fa \
+    && \
+    ruby $scripts/dereplicate_sequences.rb $outdir/two_assemblies.ryan_merged.fa > $outdir/tmp && mv $outdir/tmp $outdir/two_assemblies.ryan_merged.unique.fa
+
 
 # the minimus2 script breaks if headers are not unique
 
